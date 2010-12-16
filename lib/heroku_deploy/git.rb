45cf3e6cd3d5ad846dcbe8227495ef4dbdeedf24
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2010-12-16 10:30:09 +0000

    created git, hoptoad, packer and config

--- lib/heroku_deploy/git.rb
+++ lib/heroku_deploy/git.rb
@@ -4,6 +4,10 @@ module HerokuDeploy
       def current_version
         `git show HEAD | head -n 1`.split(" ").last.strip
       end
+      
+      def push
+        system %(git push >/dev/null 2>&1)
+      end
     end
   end
 end

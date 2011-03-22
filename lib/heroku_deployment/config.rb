dd1318b62764964344125ce194aaccbbefa2e478
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2011-03-22 11:41:19 +0000

    add option to skip deploy, for testing

--- lib/heroku_deployment/config.rb
+++ lib/heroku_deployment/config.rb
@@ -5,7 +5,8 @@ module HerokuDeployment
         yield self
       end
 
-      [:app, :remote, :generate_url, :js_files, :css_files, :commit_files, :compress_js, :compress_css, :migrate, :hoptoad, :skip_push_to_heroku].each do |meth|
+      [:app, :remote, :generate_url, :js_files, :css_files, :commit_files, :compress_js,
+        :compress_css, :migrate, :hoptoad, :skip_push_to_heroku].each do |meth|
         class_eval <<-END
           @@#{meth} = nil
           def #{meth}
